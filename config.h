96521554ed08b3b8d54214fefed038e4c923bdec
sircoolio <ericklein@users.noreply.github.com>
2024-01-18 11:35:58 -0800

    esp32-s3 feather support
    Changing dev platform to Adafruit esp32-s3 reverse feather. This requires support for ST7789 display and MAX1704X battery gauge.
    
    work in progress. This code compiles and runs properly, but UI is nascent.

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 
 // Configuration Step 2: Set debug parameters
 // comment out to turn off; 1 = summary, 2 = verbose
-// #define DEBUG 1
+#define DEBUG 1
 
 // Configuration Step 3: simulate hardware inputs, returning random but plausible values
 // comment out to turn off
@@ -22,24 +22,17 @@
   const uint16_t sensorCO2Max =       3000;
 
   const uint16_t batterySimVoltageMin = 370; // will be divided by 100.0 to give floats
-  const uint16_t batterySimVoltageMax = 410;
+  const uint16_t batterySimVoltageMax = 420;
 #endif
-
-// Configuration Step 4: Set battery parameters, if applicable
-// If LC709203F detected on i2c, define battery pack based on settings curve from datasheet
-// #define BATTERY_APA 0x08 // 100mAH
-// #define BATTERY_APA 0x0B // 200mAH
-#define BATTERY_APA 0x10 // 500mAH
-// #define BATTERY_APA 0x19 // 1000mAH
-// #define BATTERY_APA 0x1D // 1200mAH
-// #define BATTERY_APA 0x2D // 2000mAH
-// #define BATTERY_APA 0x32 // 2500mAH
-// #define BATTERY_APA 0x36 // 3000mAH
 
 // Configuration variables that change rarely
 
 // Display
-const uint8_t displayRotation = 1; // rotation 0 orients XXX
+const uint8_t displayRotation = 3; // rotation 3 orients 0,0 next to D0 button
+
+// Battery
+const float batteryVoltageMinAlert = 3.7;
+const float batteryVoltageMaxAlert = 4.2;
 
 // CO2 
 //sample timing
@@ -53,31 +46,6 @@ const uint8_t displayRotation = 1; // rotation 0 orients XXX
   const uint16_t sensorSampleInterval = 180;
 #endif
 const String co2Labels[5]={"Good", "OK", "So-So", "Poor", "Bad"};
-
-// Battery
-// analog pin used to reading battery voltage
-#define BATTERY_VOLTAGE_PIN A13 // ESP32V2
-// number of analog pin reads sampled to average battery voltage
-const uint8_t   batteryReadsPerSample = 5;
-// battery charge level lookup table
-const float batteryVoltageTable[101] = {
-  3.200,  3.250,  3.300,  3.350,  3.400,  3.450,
-  3.500,  3.550,  3.600,  3.650,  3.700,  3.703,
-  3.706,  3.710,  3.713,  3.716,  3.719,  3.723,
-  3.726,  3.729,  3.732,  3.735,  3.739,  3.742,
-  3.745,  3.748,  3.752,  3.755,  3.758,  3.761,
-  3.765,  3.768,  3.771,  3.774,  3.777,  3.781,
-  3.784,  3.787,  3.790,  3.794,  3.797,  3.800,
-  3.805,  3.811,  3.816,  3.821,  3.826,  3.832,
-  3.837,  3.842,  3.847,  3.853,  3.858,  3.863,
-  3.868,  3.874,  3.879,  3.884,  3.889,  3.895,
-  3.900,  3.906,  3.911,  3.917,  3.922,  3.928,
-  3.933,  3.939,  3.944,  3.950,  3.956,  3.961,
-  3.967,  3.972,  3.978,  3.983,  3.989,  3.994,
-  4.000,  4.008,  4.015,  4.023,  4.031,  4.038,
-  4.046,  4.054,  4.062,  4.069,  4.077,  4.085,
-  4.092,  4.100,  4.111,  4.122,  4.133,  4.144,
-  4.156,  4.167,  4.178,  4.189,  4.200};
 
 // Hardware
 // Sleep time in seconds if hardware error occurs
