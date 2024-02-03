39d9f14a720b330dcfd775d49eed427a25cc7769
sircoolio <ericklein@users.noreply.github.com>
2024-02-02 17:06:23 -0800

    Delete secrets.h

--- secrets.h
+++
@@ -1,9 +0,0 @@
-/*
-  Project:			rco2
-  Description:	contains private configuration data not stored in github
-*/
-
-// Configuration Step 1: site environment variables
-// set site altitude, in meters, to calibrate the SCD40
-const uint16_t SITE_ALTITUDE = 90; // Mercer Island, WA
-// const uint16_t SITE_ALTITUDE = 236; // Pasadena, CA (SuperCon!)
